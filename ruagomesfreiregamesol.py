d5872efd0f050080a1a4b0e9423c5b57d45b88fd
filipesousaist <filipe.miguel.sousa@tecnico.ulisboa.pt>
2019-10-11 17:01:31 +0100

    Expand a funcionar
    Falta implementar a fila de prioridades (priority queue) para a fronteira

--- ruagomesfreiregamesol.py
+++ ruagomesfreiregamesol.py
@@ -7,19 +7,11 @@ class State:
     def __init__(self, positions, tickets):
         self.positions = positions
         self.tickets = tickets
-    
-    def __eq__(self, state):
-        return self.positions == state.positions and \
-               self.tickets == state.tickets
 
 class Action:
     def __init__(self, ticketsUsed, newPositions):
         self.ticketsUsed = ticketsUsed
         self.newPositions = newPositions
-    
-    def __eq__(self, action):
-        return self.ticketsUsed == action.ticketsUsed and \
-               self.newPositions == action.newPositions
 
 class SearchProblem:
     def __init__(self, goal, model, auxheur = []):
@@ -31,7 +23,8 @@ class SearchProblem:
             self.distances.append(bfs(self.transitions, v))
 
     def search(self, init, limitexp = 2000, limitdepth = 10, tickets = [math.inf, math.inf, math.inf], anyorder = False):
-        self.initialState = State(init, tickets)   # Estado inicial (posições, bilhetes)
+        self.initialState = State(init, tickets) # Estado inicial (posições, bilhetes)
+        self.initialAction = Action([], init) # Ação inicial
         self.numPolicemen = len(init) # Nº de polícias
         result = treeSearch(self)
         return result
@@ -39,11 +32,45 @@ class SearchProblem:
     def getPossibleActions(self, state):
         # Guardar as ações que cada polícia pode fazer
         actionsByPoliceman = [self.transitions[pos] for pos in state.positions]
+
+        # Lista de combinações possíveis de ações no formato ([transporte1, pos1], [transporte2, pos2], ...) 
+        # Ex.: ([0, 20], [1, 36], [0, 37])
         actionCombinations = list(product(*actionsByPoliceman))
+
+        # Remover as ações inválidas
+        validActionCombinations = []
+        for comb in actionCombinations:
+            ticketsCopy = state.tickets[:]
+            for action in comb: # Descontar os bilhetes usados
+                ticketsCopy[action[0]] -= 1
+            validTickets = True
+            for ticketType in ticketsCopy: # Se forem usados mais bilhetes de um tipo
+                if ticketType < 0:         # do que aqueles que existem, a ação é inválida
+                    validTickets = False
+                    break
+            if not validTickets:
+                continue
+            
+            validPositions = True
+            for i in range(len(comb)):
+                for j in range(i + 1, len(comb)):
+                    if comb[i][1] == comb[j][1]: # Dois polícias querem ir para a mesma posição -> Ação inválida
+                        validPositions = False
+                        break
+                if not validPositions:
+                    break
+            if not validPositions: 
+                continue
+
+            validActionCombinations.append(comb)
+
+        # Lista de ações no formato Action([transporte1, transporte2, ...], [pos1, pos2, ...])
+        # Ex.: ([0, 1, 0], [20, 36, 37])
         possibleActions = [Action(
             [action[0] for action in actionCombination],
             [action[1] for action in actionCombination])
-            for actionCombination in actionCombinations]
+            for actionCombination in validActionCombinations]
+        
         return possibleActions
 
     def result(self, state, action):
@@ -52,7 +79,7 @@ class SearchProblem:
         for t in action.ticketsUsed:
             tickets[t] -= 1 # Descontar os bilhetes usados para ir para a nova posição
 
-        return State(action, tickets)
+        return State(action.newPositions, tickets)
 
     def tracebackPath(self, node):
         if node.parent:
--- treeSearch.py
+++ treeSearch.py
@@ -5,26 +5,28 @@ class Node:
         self.action = action
         self.pathCost = pathCost
 
-    def expand(self):
-        return []
+    def expand(self, problem):
+        children = []
+        for action in problem.getPossibleActions(self.state):
+            children.append(self.childNode(problem, action))
+        return children
 
     def childNode(self, problem, action):
         return Node(
             problem.result(self.state, action),       # Novo estado
             self,                                     # O pai do filho é o próprio nó
             action,                                   # Ação usada para gerar o filho
-            self.pathCost + 1)                        # O novo custo é o custo
+            self.pathCost + 1)                        # O novo custo é o custo deste nó + 1
 
 def extractNode(frontier):
     return frontier.pop()
 
 def treeSearch(problem):
-    frontier = [Node(problem.initialState, None,
-        Action([], problem.initialState.positions), 0)]
+    frontier = [Node(problem.initialState, None, problem.initialAction, 0)]
     while True:
         if not frontier:                         # Retornar [] se a fronteira ficar vazia
             return []
         node = extractNode(frontier)             # Escolher nó da fronteira de expansão
         if problem.isGoal(node.state):           # Se o nó tiver o estado objetivo,
             return problem.tracebackPath(node)   # devolver o caminho encontrado
-        frontier += node.expand()                # Expandir o nó atual
+        frontier += node.expand(problem)         # Expandir o nó atual
