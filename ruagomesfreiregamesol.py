3884697638e5dcb33da3732c50a81a5bcfc1eec7
filipesousaist <filipe.miguel.sousa@tecnico.ulisboa.pt>
2019-10-15 23:33:10 +0100

    tudo num ficheiro

--- ruagomesfreiregamesol.py
+++ ruagomesfreiregamesol.py
@@ -1,7 +1,24 @@
 import math
-from itertools import product
-from bfs import bfs
-from treeSearch import treeSearch
+from itertools import product, permutations
+
+def bfs(transitions, src):
+    N = len(transitions)
+
+    vertices = [src]
+    distance = [math.inf] * N
+    distance[src] = 0
+
+    for i in range(N - 1):
+        v = vertices[0]
+        vertices = vertices[1:]
+
+        for t in transitions[v]:
+            child = t[1] # Transições são da forma (tipo_de_bilhete, vértice)
+            if distance[child] == math.inf:
+                distance[child] = distance[v] + 1
+                vertices.append(child)
+
+    return distance
 
 class State:
     def __init__(self, positions, tickets):
@@ -12,6 +29,80 @@ class Action:
     def __init__(self, ticketsUsed, newPositions):
         self.ticketsUsed = ticketsUsed
         self.newPositions = newPositions
+
+class Node:
+    def __init__(self, state, parent, action, pathCost, problem):
+        self.state = state
+        self.parent = parent
+        self.action = action
+        self.pathCost = pathCost
+        self.totalCost = self.calculateTotalCost(problem)
+
+    def expand(self, problem):
+        children = []
+        for action in problem.getPossibleActions(self.state):
+            children.append(self.childNode(problem, action))
+        return children
+
+    def childNode(self, problem, action):
+        return Node(
+            problem.result(self.state, action),       # Novo estado
+            self,                                     # O pai do filho é o próprio nó
+            action,                                   # Ação usada para gerar o filho
+            self.pathCost + 1)                        # O novo custo é o custo deste nó + 1
+    
+    def calculateTotalCost(self, problem):
+        estimatedDistance = problem.distance[]
+        # f(n) =   g(n)      +    h(n)
+        return self.pathCost + estimatedDistance
+
+
+class MinPriorityQueue:
+    def __init__(self):
+        self.nodes = []
+    
+    def pop(self):
+        l = len(self.nodes)
+        if l == 0:
+            return None
+        elif l == 1:
+            return self.nodes.pop()
+        
+        firstNode = self.nodes[0]
+        self.nodes[0] = self.nodes.pop()
+        self.fixSubtree(0)
+
+        return firstNode
+
+    def fixSubtree(self, root):
+        size = len(self.nodes)
+        
+        left = 2 * root + 1
+        right = 2 * root + 2
+        largest = root
+        largestVal = self.nodes[largest].totalCost
+
+        if left < size:
+            leftVal = self.nodes[left].totalCost
+            if leftVal < rootVal:
+                largest = left
+
+    def exchange(self, n1, n2):
+        temp = self.n1
+        self.n1 = self.n2
+        self.n2 = temp
+
+def treeSearch(problem):
+    frontier = PriorityQueue()
+    frontier = [Node(problem.initialState, None, problem.initialAction, 0)]
+    while True:
+        if not frontier:                         # Retornar [] se a fronteira ficar vazia
+            return []
+        node = extractNode(frontier)             # Escolher nó da fronteira de expansão
+        if problem.isGoal(node.state):           # Se o nó tiver o estado objetivo,
+            return problem.tracebackPath(node)   # devolver o caminho encontrado
+        for child in node.expand(problem):       # Expandir o nó atual
+
 
 class SearchProblem:
     def __init__(self, goal, model, auxheur = []):
@@ -87,5 +178,10 @@ class SearchProblem:
         return [[[], node.state.positions]]
     
     def isGoal(self, state):
+        if self.anyorder:
+            for p in permutations(self.goal, self.numPolicemen):
+                if state.positions == list(p):
+                    return True
+            return False
         return state.positions == self.goal
 
